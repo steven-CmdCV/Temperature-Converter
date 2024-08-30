a8f9a31633ec70a8264275c75e2a71e3c6ff1d22
Steven <stevenjoelmanzueta@gmail.com>
2024-08-29 20:58:38 -0400

    Add files via upload

---
+++ temperatureConv.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+float fahrenheit;
+float celsius;
+int opcion;
+bool menu = true;
+
+while (menu){
+
+    cout << "TEMPERATURE CONVERTER PAGE\n";
+    cout << "----------------------------\n";
+    cout << "1. Fahrenheit(F) to Celsius(C)\n";
+    cout << "2. Celsius(C) to Fahrenheit(F)\n";
+    cout << "3. Exit\n\n";
+
+    cout << "Choose the temperature you would like to convert: ";
+    cin >> opcion;
+
+    if (opcion == 1) {
+
+        cout << "Introduce the Fahrenheit value: ";
+        cin >> fahrenheit;
+        cout << endl;
+        cout << fahrenheit << " degrees Fahrenheit is equal to " << (fahrenheit - 32) *  5 / 9 << " degrees Celsius\n\n";
+    }
+
+    else if (opcion == 2) {
+
+        cout << "Introduce the Celsius value: ";
+        cin >> celsius;
+        cout << endl;
+        cout << celsius << " degrees Celsius is equal to " << (celsius * 9 / 5) + 32 << " degrees Fahrenheit\n\n";
+        
+    }
+
+    else if (opcion == 3) {
+        menu = false;
+        cout << "\nYou have exited successfully.\n";
+    }
+ 
+}
+
+    return 0;
+}
